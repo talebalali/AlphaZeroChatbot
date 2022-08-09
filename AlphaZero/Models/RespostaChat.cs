a1bdd465b2c78d1aa141deac47304888f5ed88a7
GAMBLER <GAMBLER@DESKTOP-TFALF0C>
2022-08-09 00:40:26 -0700

    Add project files.

---
+++ AlphaZero/Models/RespostaChat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlphaZero.Models
+{
+    [Table("Chat")]
+    public class RespostaChat
+    {
+        [Column("Id")]
+        [Display(Name = "chatID")]
+        public int Id { get; set; }
+
+        [Column("Resposta")]
+        [Display(Name = "Resposta")]
+        public string Resposta { get; set; }
+
+        [Column("Message")]
+        [Display(Name = "Message")]
+        public string Message { get; set; }
+
+    }
+}
---
+++ AlphaZero/wwwroot/js/JavaScript.js
@@ -0,0 +1,30 @@
+﻿
+$(function () {
+
+    $("#Enviar").click(
+        function () {
+
+            var message = $("#message").val();
+
+            var stringUrl = "api/Chat";
+
+            $.ajax({
+                type: "POST",
+                url: stringUrl,
+                async: false,
+                data: { message: message },
+
+                success: function (data) {
+
+                    $("#displaymessage").append(" >> EU : " + message + "\n");
+                    $("#displaymessage").append(data.resposta + "\n");
+
+                    $("#message").val("")
+                }
+            });
+
+
+        }
+    );
+
+});
